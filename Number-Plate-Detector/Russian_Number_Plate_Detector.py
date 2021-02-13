a9fa0f3d1ccc09b03d6cc523f5f49f4bd7d81996
Shailesh Kumar <69575533+ShaileshKumar97@users.noreply.github.com>
2021-02-13 18:08:35 +0530

    Update Russian_Number_Plate_Detector.py

--- Number-Plate-Detector/Russian_Number_Plate_Detector.py
+++ Number-Plate-Detector/Russian_Number_Plate_Detector.py
@@ -30,7 +30,7 @@ while True:
     cv2.imshow("Result", img)
 
     if cv2.waitKey(1) & 0xFF == ord('s'):
-        cv2.imwrite("Scanned\NoPlate_"+str(count)+".jpg",imgRoi)
+        cv2.imwrite(r"Scanned\NoPlate_"+str(count)+".jpg",imgRoi)
         cv2.rectangle(img,(0,200),(640,300),(0,255,0),cv2.FILLED)
         cv2.putText(img,"Scan Saved",(150,265),cv2.FONT_HERSHEY_DUPLEX,2,(0,0,255),2)
         cv2.imshow("Result",img)
